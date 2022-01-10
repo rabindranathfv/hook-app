1ce1948c27506faa1b2555ba2d4a24ad682a989e
Rabindranath Ferreira <ferreiralox@gmail.com>
2022-01-10 16:58:24 +0100

    severals action for state with useState hooks with state compose

--- src/components/useState/CounterApp.js
+++ src/components/useState/CounterApp.js
@@ -1,14 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './counter.css';
 
 export const CounterApp = () => {
+
+    const initialState = { counter: 0, counter2: 20 };
+    const [state, setState] = useState(initialState);
+
+    const { counter, counter2 } = state;
+
+    const add = () => {
+        setState({
+            ...state,
+            counter: counter + 1
+        })
+    }
+
+    const subs = () => {
+        setState({
+            ...state,
+            counter: counter - 1
+        })
+    }
+
+    const reset = () => {
+        setState({
+            ...state,
+            counter: 0
+        })
+    }
+
     return (
         <div>
-            <h1> Counter { 0 } </h1>
+            <h1> Counter1 { counter } </h1>
+            <h1> Counter2 { counter2 } </h1>
             <hr />
 
-            <button type="button" class="btn btn-outline-primary"> +1</button>
-            <button type="button" class="btn btn-outline-primary"> -1</button>
-            <button type="button" class="btn btn-outline-primary"> reset</button>
+            <button type="button" class="btn btn-outline-primary" onClick={add}> +1</button>
+            <button type="button" class="btn btn-outline-primary" onClick={subs}> -1</button>
+            <button type="button" class="btn btn-outline-primary" onClick={reset}> reset</button>
             <button type="button" class="btn btn-outline-primary"> +1</button>
         </div>
     )
