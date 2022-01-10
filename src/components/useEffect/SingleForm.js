50bcc31505bc24ca6df45437b6bea2fe1d22a08d
Rabindranath Ferreira <ferreiralox@gmail.com>
2022-01-10 17:32:19 +0100

    use form with state and checking useEffects with array call

---
+++ src/components/useEffect/SingleForm.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import './effect.css';
+
+export const SingleForm = () => {
+
+    const [formState, setFormState] = useState({
+        name: '',
+        email: ''
+    })
+
+    const { name, email } = formState;
+
+    useEffect(() => {
+        console.log('hey!!')
+        
+    }, []);
+
+    const handleInputChange = (e) => {
+        setFormState({
+            ...formState,
+            [ e.target.name ] : e.target.value
+        })
+    };
+
+    return (
+        <div>
+            <h1> useEffect </h1>
+            <hr />
+
+            <div className="form-group">
+                <label htmlFor=""></label>
+                <input type="text"
+                className="form-control" name="name" id="" aria-describedby="name-form" placeholder="enter your name" value={name} onChange={handleInputChange}/>
+                <small className="form-text text-muted">Name</small>
+
+                <label htmlFor=""></label>
+                <input type="text"
+                className="form-control" name="email" id="" aria-describedby="email-form" placeholder="enter your email" value={email}/>
+                <small className="form-text text-muted">Email</small>
+            </div>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { HookApp } from './HookApp';
-import { CounterApp } from './components/useState/CounterApp'
+import { CounterApp } from './components/useState/CounterApp';
+import { SingleForm } from './components/useEffect/SingleForm';
 
 ReactDOM.render(
   <React.StrictMode>
-    <CounterApp />
+    <SingleForm />
   </React.StrictMode>,
   document.getElementById('root')
 );
