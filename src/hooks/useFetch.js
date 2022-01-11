30ca9aa8c618a210064bed55c846d9f56b0d60c4
Rabindranath Ferreira <ferreiralox@gmail.com>
2022-01-11 17:14:14 +0100

    add loading transition for useFetch when search next quote

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -5,6 +5,9 @@ export const useFetch = (url) => {
     const [state, setsState] = useState({ data: null, loading: true, error: null})
 
     useEffect(() => {
+
+        setsState({ data: null, loading: true, error: null})
+
         fetch( url ).then( resp => resp.json()).then( data => {
             setsState({
                 loading: false,
