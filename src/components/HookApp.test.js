3e1d900c4a8eb256e6d26615aa4cc144739302c0
Rabindranath Ferreira <ferreiralox@gmail.com>
2022-01-19 02:48:07 +0100

    add config for setup and unit test for HookApp

---
+++ src/components/HookApp.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { HookApp } from '../HookApp';
+
+describe('HookApp Component::', () => {
+    
+    it('should render HookApp', () => {
+        const wrapper = shallow(<HookApp />);
+        expect(wrapper).toMatchSnapshot();
+    });
+})
