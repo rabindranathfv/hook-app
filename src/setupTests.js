2d5db349d4db77520518862ee8c2ebf9c4107b13
Rabindranath Ferreira <ferreiralox@gmail.com>
2022-01-19 02:29:34 +0100

    configure test for jest, enzyme, and adapter enzyme17

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,12 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// imports testing using ENZYME
+import {createSerializer} from 'enzyme-to-json';
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import "jest-enzyme";
+
+Enzyme.configure({ adapter: new Adapter() });
+expect.addSnapshotSerializer( createSerializer({mode: 'deep'}));
